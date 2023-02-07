0537f396a03cc5b5c8704b7b220cb2f582a13c01
henriquemaria <henrm@riseup.net>
2023-02-07 11:36:47 +0100

    Add Raleway font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { Raleway } from "@next/font/google";
+import type { AppProps } from "next/app";
+
+const raleway = Raleway({
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={raleway.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
