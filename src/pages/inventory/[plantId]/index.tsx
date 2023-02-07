9231bc025176ff57c3782c7e7838033275c487e8
henriquemaria <henrm@riseup.net>
2023-02-07 12:34:24 +0100

    Add pages "/", "/inventory", "/inventory/[plantId]"

---
+++ src/pages/inventory/[plantId]/index.tsx
@@ -0,0 +1,6 @@
+import Layout from "@/components/Layout";
+import React from "react";
+
+export default function Flower() {
+  return <Layout>Flower...</Layout>;
+}
---
+++ src/pages/inventory/index.tsx
@@ -0,0 +1,6 @@
+import Layout from "@/components/Layout";
+import React from "react";
+
+export default function inventory() {
+  return <Layout>Inventory list...</Layout>;
+}
